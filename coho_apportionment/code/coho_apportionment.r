ddd1d88c119f38579c5c384b773efa47cce5d9c6
fssem1 <sara.miller@alaska.gov>
2025-11-19 13:43:14 -0900

    commit data

--- coho_apportionment/code/coho_apportionment.r
+++ coho_apportionment/code/coho_apportionment.r
@@ -6,10 +6,10 @@ library(tidyr)
 source('coho_apportionment/code/missfill_functions.r')
 
 # data ----
-read.csv('coho_apportionment/data/chilkat_coho.csv')-> situk
+read.csv('coho_apportionment/data/chilkat_coho.csv')-> chilkat
 
 # pivot year columns into long format for import into functions
-df_long <- situk %>%
+df_long <- chilkat %>%
   pivot_longer(
     cols = -date,    # all year columns now start with 'x'
     names_to = "year",
@@ -17,8 +17,8 @@ df_long <- situk %>%
   mutate(year = as.numeric(sub("X", "", year)))
 
 # run functions ----
-impute_global(df_long)
-impute_local(df_long)
+impute_global(df_long) #imputes all NAs in all years iteratively
+impute_local(df_long) #imputes a 10-year rolling imputation (prev & following 5 years)
 
 # read and save output in wide format----
 read.csv("coho_apportionment/output/global_imputed.csv") %>%
